73064d6c79bc24bd6ffdb92a7fd13490b104807d
David Llanos <davidllanos22@gmail.com>
2015-03-18 22:40:24 +0100

    Display working. kinda... :joy:

--- src/emulator.js
+++ src/emulator.js
@@ -4,8 +4,8 @@ class Emulator {
     this.chip8 = new Chip8();
     this.cvs = document.getElementById("canvas");
     this.ctx = this.cvs.getContext("2d");
-    this.cvs.width = 64;
-    this.cvs.height = 32;
+    this.cvs.width = 640;
+    this.cvs.height = 320;
   }
   load(){
     var xhr = new XMLHttpRequest;
@@ -27,13 +27,13 @@ class Emulator {
     setTimeout(()=> {
       this.chip8.emulateCycle();
       updateRegistersDisplay();
-      this.ctx.fillStyle = "#222222";
-      this.ctx.fillRect(0, 0, 64, 32);
-      this.ctx.fillStyle = "#444444";
+      this.ctx.fillStyle = "#000";
+      this.ctx.fillRect(0, 0, 640, 320);
+      this.ctx.fillStyle = "#FFF";
       for(var x = 0; x < 64; x++){
         for(var y = 0; y < 32; y++){
           if(this.chip8.screen[x + (y * 64)])
-            this.ctx.fillRect(x * 10, y * 10, 10, 10);
+            this.ctx.fillRect(x*10, y*10, 10, 10);
         }
       }
 
